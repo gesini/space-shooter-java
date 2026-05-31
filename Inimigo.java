41107dd59d87d7597e499c1c873088b334e6d306
gesini <d.gesininascimento@gmail.com>
2026-05-31 19:39:33 -0300

    melhorar este file

--- Inimigo.java
+++ Inimigo.java
@@ -2,30 +2,33 @@ import java.awt.Rectangle;
 import java.util.Random;
 
 public class Inimigo {
-    private int x, y, dx = 2;
+    private int x, y, dx;
     private Random r = new Random();
 
-    public Inimigo(){
+    // Agora o inimigo sabe em qual fase está e fica mais rápido!
+    public Inimigo(int fase){
+        this.dx = 2 + fase; // Fase 1 = vel 3, Fase 2 = vel 4...
         respawn();
     }
 
     public void mover(){
         x += dx;
         if(x < 0 || x > 760)
-            dx *= -1;
+            dx *= -1; // Inverte a direção ao bater na parede
     }
 
     public boolean deveAtirar(){
+        // Chance de tiro baseada em probabilidade
         return r.nextInt(100) < 2;
     }
 
     public void respawn(){
         x = r.nextInt(700);
-        y = r.nextInt(300);
+        y = r.nextInt(300); // Surge na metade superior da tela
     }
 
     public Rectangle getBounds(){
-        return new Rectangle(x,y,40,40);
+        return new Rectangle(x, y, 40, 40);
     }
 
     public int getX(){ return x; }
